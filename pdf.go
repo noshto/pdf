0d6172494e50e76c268a5aee860a10c496e7b5ff
Valerii Lider <valerii.lider@gmail.com>
2021-02-18 22:46:42 +0100

    pdf generation adjusted to render more or less the same invoice as from example

--- pdf.go
+++ pdf.go
@@ -91,29 +91,24 @@ func GeneratePDF(params *Params) error {
 	b64Image := base64.StdEncoding.EncodeToString(buf)
 
 	// Company Name along with QR code
-	m.Row(32, func() {
-		m.Col(10, func() {
+	m.Row(20, func() {
+		m.Col(12, func() {
 			m.Text(request.Invoice.Seller.Name, TitleTextAttrib)
 		})
 
-		m.Col(2, func() {
+		m.Row(12, func() {})
+		// m.Col(2, func() {
 
-			m.Base64Image(b64Image, consts.Png, props.Rect{
-				Left:    0,
-				Top:     0,
-				Percent: 100,
-				Center:  true,
-			})
-			// m.FileImage(qrCodeFile, props.Rect{
-			// 	Left:    0,
-			// 	Top:     0,
-			// 	Percent: 100,
-			// 	Center:  true,
-			// })
-		})
+		// 	m.Base64Image(b64Image, consts.Png, props.Rect{
+		// 		Left:    0,
+		// 		Top:     0,
+		// 		Percent: 100,
+		// 		Center:  true,
+		// 	})
+		// })
 
-		// Adds extra space from Company name to next lines
-		m.Row(20, func() {})
+		// // Adds extra space from Company name to next lines
+		// m.Row(10, func() {})
 
 		// Company details
 		m.Row(4, func() {
@@ -143,14 +138,23 @@ func GeneratePDF(params *Params) error {
 		})
 	})
 
+	m.Line(6)
+
 	// Invoice number on the left with Buyer header on the right
-	m.Row(6, func() {
+	m.Row(4, func() {
 		m.Col(6, func() {
-			m.Text(strings.Join([]string{"Broj računa:", string(request.Invoice.InvNum)}, " "), CaptionTextAttrib)
-			m.Text(strings.Join([]string{"Interni br:", string(params.InternalInvNum)}, " "), CaptionTextAttrib)
+			m.Text(strings.Join([]string{"Broj racuna:", string(request.Invoice.InvNum)}, " "), CaptionTextAttrib)
 		})
 		m.Col(6, func() {
 			m.Text("KUPAC", CaptionTextAttrib)
+		})
+	})
+	m.Row(4, func() {
+		m.Col(6, func() {
+			m.Text(strings.Join([]string{"Interni br:", string(params.InternalInvNum)}, " "), CaptionTextAttrib)
+		})
+		m.Col(6, func() {
+			m.Text(request.Invoice.Buyer.Name, CaptionTextAttrib)
 		})
 	})
 
@@ -160,8 +164,10 @@ func GeneratePDF(params *Params) error {
 			IssueDateTime := time.Time(request.Invoice.IssueDateTime).Format("2006-01-02")
 			m.Text(strings.Join([]string{"Datum prometa dobara:", IssueDateTime}, " "), BodyTextAttrib)
 		})
+		addressString := strings.Join([]string{request.Invoice.Buyer.Address, request.Invoice.Buyer.Town, request.Invoice.Buyer.Country}, ", ")
+		addressString = strings.TrimRight(addressString, ", ")
 		m.Col(6, func() {
-			m.Text(request.Invoice.Buyer.Name, CaptionTextAttrib)
+			m.Text(strings.Join([]string{"Adresa:", addressString}, " "), BodyTextAttrib)
 		})
 	})
 
@@ -174,11 +180,8 @@ func GeneratePDF(params *Params) error {
 			}
 			m.Text(strings.Join([]string{"Valuta:", string(Currency)}, " "), BodyTextAttrib)
 		})
-
-		addressString := strings.Join([]string{request.Invoice.Buyer.Address, request.Invoice.Buyer.Town, request.Invoice.Buyer.Country}, ", ")
-		addressString = strings.TrimRight(addressString, ", ")
 		m.Col(6, func() {
-			m.Text(strings.Join([]string{"Adresa:", addressString}, " "), BodyTextAttrib)
+			m.Text(strings.Join([]string{"PIB:", request.Invoice.Buyer.IDNum}, " "), BodyTextAttrib)
 		})
 	})
 	// Payment type on the left and Buyer TIN on the right
@@ -193,13 +196,6 @@ func GeneratePDF(params *Params) error {
 			}
 			m.Text(strings.Join([]string{"Nacin placanja:", TypeOfInv}, " "), BodyTextAttrib)
 		})
-		m.Col(6, func() {
-			m.Text(strings.Join([]string{"PIB:", request.Invoice.Buyer.IDNum}, " "), BodyTextAttrib)
-		})
-	})
-	// Spece on the left and Buyer's VAT on the right
-	m.Row(4, func() {
-		m.ColSpace(6)
 		m.Col(6, func() {
 			client := &sep.Client{}
 			for _, it := range params.SepConfig.Clients {
@@ -216,33 +212,69 @@ func GeneratePDF(params *Params) error {
 
 	content := [][]string{}
 	for in, it := range *request.Invoice.Items {
+
+		q := float64(it.Q)
+		upb := float64(it.UPB)
+		vr := float64(it.VR)
+		r := float64(it.R)
+
+		// Calculations
+		// upbr is for "Unit Price Before VAT, Rabat applied"
+		upbr := (upb - upb*(r/100))
+		// pb is for "Price Before VAT"
+		pb := upbr * q
+
+		// uva is for "Unit VAT Amount, Rabat applied"
+		uva := upbr * (vr / 100)
+		// va is for "VAT Amount"
+		va := uva * q
+
+		// upa is for "Unit Price After VAT, Rabat applied"
+		upa := upbr + uva
+		// pa is for "Price After VAT, Rabat applied"
+		pa := pb + va
+
+		Name := it.N
+		Unit := it.U
+		Quantity := strconv.FormatFloat(float64(it.Q), 'f', 2, 64)
+		UnitPriceBefVAT := strconv.FormatFloat(upb, 'f', 2, 64)
+		PriceBefVAT := strconv.FormatFloat(upb*q, 'f', 2, 64)
+		Rebate := fmt.Sprintf("%d%%", int64(r))
+		VATRate := fmt.Sprintf("%d%%", int64(vr))
+		VATAmount := strconv.FormatFloat(va, 'f', 2, 64)
+		UnitPriceAfterVAT := strconv.FormatFloat(upa, 'f', 2, 64)
+		PriceAfterVAT := strconv.FormatFloat(pa, 'f', 2, 64)
+
 		content = append(
 			content,
 			[]string{
 				strconv.Itoa(in + 1),
-				it.N,
-				it.U,
-				strconv.FormatFloat(float64(it.Q), 'f', 2, 64),
-				strconv.FormatFloat(float64(it.UPB), 'f', 2, 64),
-				strings.Join([]string{strconv.FormatFloat(float64(it.R), 'f', 2, 64), "%"}, " "),
-				strings.Join([]string{strconv.FormatFloat(float64(it.VR), 'f', 2, 64), "%"}, " "),
-				strconv.FormatFloat(float64(it.PA), 'f', 2, 64),
+				Name,
+				Unit,
+				Quantity,
+				UnitPriceBefVAT,
+				PriceBefVAT,
+				Rebate,
+				VATRate,
+				VATAmount,
+				UnitPriceAfterVAT,
+				PriceAfterVAT,
 			},
 		)
 	}
 
 	m.TableList(
-		[]string{"#", "NAZIV PROIZVODA/USLUGE", "JM", "Kolicina", "Cijena bez PDV", "Rabat %", "PDV stopa", "Cijena sa PDV"},
+		[]string{"Rb", "NAZIV PROIZVODA/USLUGE", "JM", "Kolicina", "      Cijena      bez PDV", "    Vrijednost     bez PDV", "Rabat %", "PDV Stopa", "PDV Iznos", "      Cijena       sa PDV", "    Vrijednost    sa PDV"},
 		content,
 		props.TableList{
 			HeaderProp: props.TableListContent{
-				Size:      9,
-				Style:     consts.Bold,
-				GridSizes: []uint{1, 3, 1, 1, 2, 1, 1, 2},
+				Size:      6,
+				Style:     consts.Normal,
+				GridSizes: []uint{1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 			},
 			ContentProp: props.TableListContent{
-				Size:      8,
-				GridSizes: []uint{1, 3, 1, 1, 2, 1, 1, 2},
+				Size:      6,
+				GridSizes: []uint{1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 			},
 			Align: consts.Center,
 			AlternatedBackground: &color.Color{
@@ -259,66 +291,146 @@ func GeneratePDF(params *Params) error {
 
 	m.Row(4, func() {})
 
-	TotPriceWoVAT := strconv.FormatFloat(float64(request.Invoice.TotPriceWoVAT), 'f', 2, 64)
+	PriceBeforeVAT := float64(0)
+	Rebate := float64(0)
+	VATAmt := float64(0)
+	for _, it := range *request.Invoice.Items {
+		PriceBeforeVAT += float64(it.UPB * it.Q)
+		Rebate += PriceBeforeVAT * (float64(it.R) / 100)
+		VATAmt += (PriceBeforeVAT - Rebate) * (float64(it.VR) / 100)
+	}
+	Base21 := PriceBeforeVAT - Rebate
 	TotPrice := strconv.FormatFloat(float64(request.Invoice.TotPrice), 'f', 2, 64)
-	TotVATAmt := strconv.FormatFloat(float64(request.Invoice.TotVATAmt), 'f', 2, 64)
-
-	VATRate := 21.0
-	for _, it := range *request.Invoice.SameTaxes {
-		VATRate = float64(it.VATRate)
+	if Rebate != 0 {
+		Rebate *= -1
 	}
 
 	// Invoice summary
-	m.Row(4, func() {
-		m.ColSpace(5)
-		m.Col(3, func() {
-			m.Text("Osnovica za stopu 21%:", BodyRightAlignTextAttrib)
+	m.Row(32, func() {
+		m.Col(2, func() {
+			m.Base64Image(b64Image, consts.Png, props.Rect{
+				Left:    0,
+				Top:     0,
+				Percent: 100,
+				Center:  true,
+			})
 		})
-		m.Col(4, func() {
-			m.Text(TotPriceWoVAT, BodyRightAlignTextAttrib)
+		m.ColSpace(4)
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(3, func() {
+				m.Text("Vrijednost bez PDV:", BodyRightAlignTextAttrib)
+			})
+			m.Col(3, func() {
+				m.Text(strconv.FormatFloat(PriceBeforeVAT, 'f', 2, 64), BodyRightAlignTextAttrib)
+			})
 		})
-	})
-	m.Row(4, func() {
-		m.ColSpace(5)
-		m.Col(3, func() {
-			m.Text("Iznos rabata:", BodyRightAlignTextAttrib)
-		})
-		m.Col(4, func() {
-			m.Text("0.00", BodyRightAlignTextAttrib)
-		})
-	})
 
-	m.Row(4, func() {
-		m.ColSpace(5)
-		m.Col(3, func() {
-			m.Text("Vrijednost bez PDV:", BodyRightAlignTextAttrib)
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(3, func() {
+				m.Text("Iznos rabata:", BodyRightAlignTextAttrib)
+			})
+			m.Col(3, func() {
+				m.Text(strconv.FormatFloat(Rebate, 'f', 2, 64), BodyRightAlignTextAttrib)
+			})
 		})
-		m.Col(4, func() {
-			m.Text(TotPriceWoVAT, BodyRightAlignTextAttrib)
+
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(6, func() {
+				m.Text("-----------------------------------------------------------------------------------", BodyRightAlignTextAttrib)
+			})
+		})
+
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(3, func() {
+				m.Text("Osnovica za stopu 21%:", BodyRightAlignTextAttrib)
+			})
+			m.Col(3, func() {
+				m.Text(strconv.FormatFloat(Base21, 'f', 2, 64), BodyRightAlignTextAttrib)
+			})
+		})
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(3, func() {
+				m.Text("PDV po stopi 21%:", BodyRightAlignTextAttrib)
+			})
+			m.Col(3, func() {
+				m.Text(strconv.FormatFloat(VATAmt, 'f', 2, 64), BodyRightAlignTextAttrib)
+			})
+		})
+
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(6, func() {
+				m.Text("-----------------------------------------------------------------------------------", BodyRightAlignTextAttrib)
+			})
+		})
+
+		m.Row(4, func() {
+			m.ColSpace(6)
+			m.Col(3, func() {
+				m.Text("IZNOS ZA UPLATU:", CaptionRightAlignTextAttrib)
+			})
+			m.Col(3, func() {
+				m.Text(TotPrice, CaptionRightAlignTextAttrib)
+			})
 		})
 	})
+	// m.Row(4, func() {
+	// 	m.ColSpace(6)
+	// 	m.Col(3, func() {
+	// 		m.Text("Iznos rabata:", BodyRightAlignTextAttrib)
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text(strconv.FormatFloat(Rebate, 'f', 2, 64), BodyRightAlignTextAttrib)
+	// 	})
+	// })
 
-	m.Row(4, func() {
-		m.ColSpace(5)
-		m.Col(3, func() {
-			m.Text("PDV po stopi 21%", BodyRightAlignTextAttrib)
-		})
-		m.Col(4, func() {
-			m.Text(TotVATAmt, BodyRightAlignTextAttrib)
-		})
-	})
+	// m.Row(4, func() {
+	// 	m.ColSpace(6)
+	// 	m.Col(6, func() {
+	// 		m.Text("-----------------------------------------------------------------------------------", BodyRightAlignTextAttrib)
+	// 	})
+	// })
 
-	m.Line(1)
+	// m.Row(4, func() {
+	// 	m.ColSpace(6)
+	// 	m.Col(3, func() {
+	// 		m.Text("Osnovica za stopu 21%:", BodyRightAlignTextAttrib)
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text(strconv.FormatFloat(Base21, 'f', 2, 64), BodyRightAlignTextAttrib)
+	// 	})
+	// })
+	// m.Row(4, func() {
+	// 	m.ColSpace(6)
+	// 	m.Col(3, func() {
+	// 		m.Text("PDV po stopi 21%:", BodyRightAlignTextAttrib)
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text(strconv.FormatFloat(VATAmt, 'f', 2, 64), BodyRightAlignTextAttrib)
+	// 	})
+	// })
 
-	m.Row(4, func() {
-		m.ColSpace(5)
-		m.Col(3, func() {
-			m.Text("IZNOS ZA UPLATU:", CaptionRightAlignTextAttrib)
-		})
-		m.Col(4, func() {
-			m.Text(TotPrice, CaptionRightAlignTextAttrib)
-		})
-	})
+	// m.Row(4, func() {
+	// 	m.ColSpace(6)
+	// 	m.Col(6, func() {
+	// 		m.Text("-----------------------------------------------------------------------------------", BodyRightAlignTextAttrib)
+	// 	})
+	// })
+
+	// m.Row(4, func() {
+	// 	m.ColSpace(6)
+	// 	m.Col(3, func() {
+	// 		m.Text("IZNOS ZA UPLATU:", CaptionRightAlignTextAttrib)
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text(TotPrice, CaptionRightAlignTextAttrib)
+	// 	})
+	// })
 
 	// Extra space between Invoice Summary and IKOF and JIKR codes
 	m.Row(4, func() {})
@@ -355,9 +467,40 @@ func GeneratePDF(params *Params) error {
 		})
 	})
 
-	if VATRate == 0 {
+	ExText := ""
+	for _, it := range *request.Invoice.Items {
+		switch it.EX {
+		case sep.CL17:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 17. Zakona o PDV-u"
+			break
+		case sep.CL20:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 20. Zakona o PDV-u"
+			break
+		case sep.CL26:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 26. Zakona o PDV-u"
+			break
+		case sep.CL27:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 27. Zakona o PDV-u"
+			break
+		case sep.CL28:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 28. Zakona o PDV-u"
+			break
+		case sep.CL29:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 29. Zakona o PDV-u"
+			break
+		case sep.CL30:
+			ExText = "PDV obracunat po stopi 0% u skladu sa Clanom 30. Zakona o PDV-u"
+			break
+		default:
+			continue
+		}
+	}
+	if ExText != "" {
 		m.Row(4, func() {
-			m.Text("PDV obracunat po stopi 0% u skladu sa Clanom 17. Zakona o PDV-u", BodyTextAttrib)
+			m.Text(ExText, BodyTextAttrib)
+		})
+		// extra space
+		m.Row(2, func() {
 		})
 	}
 
@@ -369,7 +512,6 @@ func GeneratePDF(params *Params) error {
 	})
 
 	m.Row(4, func() {
-
 		m.ColSpace(6)
 		m.Col(6, func() {
 			m.Text("M.P. _________________________", BodyRightAlignTextAttrib)
